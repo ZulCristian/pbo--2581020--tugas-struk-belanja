1879d0b35ee1050ea66b90e53bcfdeb26f69b7e6
Zul Cristian Purba <2581020@unai.edu>
2026-09-12 23:38:17 +0700

    menambahkan nama barang 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("Nama barang 1 : ");
+        String nama1 = input.nextLine();
+
+    }
+}
